bc1c1d1463eb4bcba230308fd12ca4b78518d1f6
Manish hooda <mhooda2412@gmail.com>
2020-04-25 16:47:02 +0530

    CommonControler implemented

--- Trello_quora/quora-service/src/main/java/com/upgrad/quora/service/business/UserBusinessService.java
+++ Trello_quora/quora-service/src/main/java/com/upgrad/quora/service/business/UserBusinessService.java
@@ -5,7 +5,9 @@ import com.upgrad.quora.service.dao.UserDao;
 import com.upgrad.quora.service.entity.UserAuthTokenEntity;
 import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.exception.AuthenticationFailedException;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.SignUpRestrictedException;
+import com.upgrad.quora.service.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -84,4 +86,22 @@ public class UserBusinessService {
         }
     }
 
+    public UserEntity getUser(final String userUuid, final String authorizationToken)
+            throws AuthorizationFailedException, UserNotFoundException {
+        UserAuthTokenEntity userAuthEntity = userDao.getUserAuthToken(authorizationToken);
+        if (userAuthEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        } else if ((userAuthEntity.getLogoutAt() != null && userAuthEntity.getLogoutAt().isBefore(ZonedDateTime.now()))
+                || userAuthEntity.getExpiresAt().isBefore(ZonedDateTime.now())) {
+            throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to get user details");
+        } else {
+            UserEntity userEntity = userDao.getUser(userUuid);
+            if (userEntity == null) {
+                throw new UserNotFoundException("USR-001", "User with entered uuid does not exist");
+            }
+            return userEntity;
+        }
+    }
 }
+
+
--- Trello_quora/quora-service/src/main/java/com/upgrad/quora/service/entity/UserEntity.java
+++ Trello_quora/quora-service/src/main/java/com/upgrad/quora/service/entity/UserEntity.java
@@ -12,6 +12,7 @@ import java.io.Serializable;
 @Entity
 @Table(name = "users")
 @NamedQueries({
+        @NamedQuery(name = "userByUuid", query = "select u from UserEntity u where u.uuid = :uuid"),
         @NamedQuery(name = "userByEmail", query = "select u from UserEntity u where u.email = :email"),
         @NamedQuery(name = "userByUserName", query = "select u from UserEntity u where u.userName = :userName")
 })
